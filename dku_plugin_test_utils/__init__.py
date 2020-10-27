7a7315553b4b01662dae85dce78e723fc9a2cc4e
Nicolas Triquenaux <nicolas.triquenaux@dataiku.com>
2020-10-27 17:53:15 +0100

    Adding docstring and altering the test run config parsing as it evolved to target many DSS instance [ch55886]

--- dku_plugin_test_utils/__init__.py
+++ dku_plugin_test_utils/__init__.py
@@ -2,10 +2,24 @@ import json
 
 
 def get_plugin_info():
+    """
+    Read the plugin information that can be found at the root level of each plugin
+
+    Returns:
+        dict: The python dict representing the plugin informations
+    """
     with open('plugin.json') as json_file:
         data = json.load(json_file)
-        return data
+    return data
 
 
 def scenario(client, project_key, scenario_id):
+    """
+    Remotly run a DSS scenario that correspond to one pytest test
+
+    Args:
+        client: DSS client instance from dataikuapi
+        project_key (str): The project holding the scenarios to run
+        scenario_id (str): The DSS scenario to run
+    """
     client.get_project(project_key).get_scenario(scenario_id).run_and_wait()
--- dku_plugin_test_utils/run_config/config_parser.py
+++ dku_plugin_test_utils/run_config/config_parser.py
@@ -3,6 +3,12 @@ import os
 
 
 class ScenarioConfiguration(object):
+    """
+    Class that will hold the test cessuib configuration regarding DSS and users that will be involded.
+    It is a singleton, so it will read only once the configuration and each time it is requested, it will
+    loaded directly from memory and not from the json file.
+    """
+
     _instance = None
 
     def __new__(cls, *args, **kwargs):
@@ -18,20 +24,25 @@ class ScenarioConfiguration(object):
 
     def __init__(self):
 
+        # Skip the init if we already have read the file.
         if self._initialized:
             return
 
+        # Get the path to the configuration file, if it is empty raise an Error
         test_instance_config_path = os.getenv("PLUGIN_INTEGRATION_TEST_INSTANCE", None)
         if not test_instance_config_path:
             raise ValueError("'PLUGIN_INTEGRATION_TEST_INSTANCE' is not defined, please point it to an instance configuration file")
-        
+
+        # Open the json file and map it to a python dict
         with open(test_instance_config_path, "r") as fd:
             self._run_instance_config = json.load(fd)
 
-    @property
-    def host(self):
-        return self._run_instance_config.get("host", None)
+        # Slightly change the format to put the key which is the target DSS
+        # at the same level of the other walues.
+        self._hosts = []
+        for key, value in self._run_instance_config.items():
+            self._hosts.append({"target": key, **value})
 
     @property
-    def api_key(self):
-        return self._run_instance_config.get("api_key", None)
+    def hosts(self):
+        return self._hosts
